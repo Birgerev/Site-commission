67e38c1f2b1bfb89580cf86a286f9790f34e1fd3
Birger E <bibbevidio@gmail.com>
2023-04-01 18:04:06 +0200

    comment

--- Site/scripts/registerForm.js
+++ Site/scripts/registerForm.js
@@ -2,5 +2,6 @@ function onClick() {
 		console.log(document.getElementById("name").value);
 		console.log(document.getElementById("password").value);
 
+		//Redirect
 		window.location.href = "index.html";
 }
