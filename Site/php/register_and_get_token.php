7790ecaf77bb9e39345aa24c7ac63c41a0a73b56
Birger E <bibbevidio@gmail.com>
2023-04-02 17:09:48 +0200

    Register accound

--- Site/php/register_and_get_token.php
+++ Site/php/register_and_get_token.php
@@ -4,16 +4,25 @@ $conn = new mysqli("localhost", "root", "", "chat");
 
 // Check connection
 if ($conn->connect_error) {
+  http_response_code(500);  //Internal error response
   die("Connection failed: " . $conn->connect_error);
 }
 
+//read user request data
 $account_name = $_GET['account_name'];
 $password = $_GET['password'];
 
+//Check if account name already exists
+$result = $conn->query("SELECT * FROM registered_accounts WHERE account_name = '$account_name'");
+
+//Return error if account already exists
+if($result->num_rows > 0){
+  http_response_code(400);  //Bad request response (cant create duplicates)
+  die("Duplicate account");
+}
 
 //Insert new account into database
 $conn->query("INSERT INTO registered_accounts (account_name, password) VALUES ('$account_name', '$password')");
-
 
 //Generate a token which user uses to validate theirself
 $token = generateRandomString(32);
--- Site/scripts/registerForm.js
+++ Site/scripts/registerForm.js
@@ -1,7 +1,30 @@
 function onClick() {
-		console.log(document.getElementById("name").value);
-		console.log(document.getElementById("password").value);
+		var name = document.getElementById("name").value;
+		var password = document.getElementById("password").value;
+
+		//Make http request to php site and await response
+		fetch("php/register_and_get_token.php?account_name=" + name + "&password=" + password)
+		.then(response => {
+      if (!response.ok) {//Throw error if http request failed
+        throw new Error(response.status);
+      }
+      return response.text();
+    })
+		.then(data => recievedToken(data))//Call recievedToken if request was Succesfull
+		.catch(error => errorResponse(error));
+}
+
+function recievedToken(token) {
+		console.log('Token: ' + token);
 
 		//Redirect
 		window.location.href = "index.html";
+
+		//TODO save cookie
+}
+
+function errorResponse(error){
+		console.error('HTML-request error: ' + error);
+
+		document.getElementById("errorText").textContent = "Misslyckades (Kontot kanske redan fanns?): " + error;
 }
