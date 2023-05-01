23bb6f6e70f7969ffdd6e53402ca282b1509f3b1
Birger E <bibbevidio@gmail.com>
2023-05-01 21:19:53 +0200

    Js comments

--- Site/scripts/chat.js
+++ Site/scripts/chat.js
@@ -1,15 +1,17 @@
-
+//Determine whether we are logged in based on if cookie is set
 var isLoggedIn = (document.cookie != "empty");
 
+//Remove all elements that should be hidden when logged in/ logged out
 if(isLoggedIn)
 		document.querySelectorAll('.visibleWhenLoggedOut').forEach(element => element.remove());
 else
 		document.querySelectorAll('.visibleWhenLoggedIn').forEach(element => element.remove());
 
+//Method called when log out button is pressed
 function onClickLogOut() {
-		//Empty the cookie, which contains token if logged in
+		//Empty the cookie, which contains the authentication token if logged in
 		document.cookie = "empty";
 
-		// Reload the current page
+		// Refresh the website
 		location.reload();
 }
